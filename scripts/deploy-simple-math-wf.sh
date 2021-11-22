33eb8c4b52ed727471d92e2b507aff42bf43d956
Adonis Legon <Adonis.Legon@sovos.com>
2021-11-22 09:12:07 -0300

    removed internal file

--- scripts/deploy-simple-math-wf.sh
+++
@@ -1,5 +0,0 @@
-aws stepfunctions update-state-machine \
---state-machine-arn arn:aws:states:sa-east-1:342542966819:stateMachine:simple-math \
---definition file://../simple-math.asl.json \
---role-arn arn:aws:iam::342542966819:role/service-role/StepFunctions-simple-math-role-48ab458e \
---profile sovos-gvat-dev
